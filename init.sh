cd34d79df91feb8fbb55cdd01039a6a0a6dd78f1
albert.sigman <albert.sigman@zenecosystems.com>
2024-11-07 23:51:30 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 pull_haptycs_gh() {
-  cd /home/pi/Desktop/haptycs-ble/ && git pull
+  cd /home/pi/Desktop/haptycs-ble/ && git -c credential.helper='!f() { echo "password=raspberry"; }; f' pull
 }
 
 echo 'Begin ping'
     wget -q --spider http://google.com
 if [ $? -eq 0 ]; then
-    echo "pulling from github the latest branch" &&
+    echo "Wifi detected, pulling from github the latest branch" &&
     pull_haptycs_gh
 
 else
