b6c30cd48095bce20127d5c154fa14ef2c320038
albert.sigman <albert.sigman@zenecosystems.com>
2024-11-07 23:38:38 -0500

    reformat

--- init.sh
+++ init.sh
@@ -3,12 +3,19 @@ pull_haptycs_gh() {
   cd /home/pi/Desktop/haptycs-ble/ && git pull
 }
 
-wifi="$(ip a | grep wlp2s0 | grep inet | wc -l)"
-                                        if [ $wifi = 1 ] || [ $wifi = 2 ]; then
-                                                echo "pulling from github the latest branch"
-                        pull_haptycs_gh
-                                        else
-                                                echo "睊 No wifi!, just running the service"
-                                        fi
+echo 'Begin ping'
+if ping -c 1 8.8.8.8 > /dev/null; then
+    echo "睊 No wifi!, just running the service"
+else
+    echo "pulling from github the latest branch" &&
+    pull_haptycs_gh
+fi
+
+
+# wifi="$(ip a | grep wlp2s0 | grep inet | wc -l)"
+#                                         if [ $wifi = 1 ] || [ $wifi = 2 ]; then
+
+#                                         else
+#                                         fi
 
 /usr/bin/python3 /home/pi/Desktop/haptycs-ble/cputemp.py
