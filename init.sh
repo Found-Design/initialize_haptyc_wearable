e95d74f890245851535e764ee33b1b8b4280d2fb
albert.sigman <albert.sigman@zenecosystems.com>
2024-11-21 23:05:11 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pull_haptycs_gh() {
-  cd /home/pi/Desktop/haptycs-ble/ && git -c credential.helper='!f() { echo "password=raspberry"; }; f' pull
+  cd /home/pi/desktop/haptycs-ble/ && git pull
 }
 
 echo 'Begin ping'
@@ -8,11 +8,11 @@ echo 'Begin ping'
 if [ $? -eq 0 ]; then
     echo "Wifi detected, pulling from github the latest branch" &&
     pull_haptycs_gh
-
 else
     echo "Offline"
     echo "睊 No wifi!, just running the service"
 fi
 
-
-/usr/bin/python3 /home/pi/Desktop/haptycs-ble/cputemp.py
+cd /home/pi/desktop/haptycs-ble && \
+. /home/pi/desktop/haptycs-ble/env/bin/activate && \
+python3 cputemp.py
